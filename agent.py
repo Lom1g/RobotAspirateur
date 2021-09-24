947a2b97562e0bc61599c89ea602b8ab76427f77
Samantha Majot <samantha.majot@gmail.com>
2021-09-24 12:07:23 -0400

    add file Agent

---
+++ agent.py
@@ -0,0 +1,19 @@
+class Agent:
+    def __init__(self):
+        pass
+
+    #attributs
+
+    #exploration non-informée
+
+    #exploration informée
+
+    #aspiration poussière
+
+    #ramassage bijoux
+
+    #déplacement
+
+    #calcul performance
+
+    #apprentissage épisodique
